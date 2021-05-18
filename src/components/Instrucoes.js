cbd6484b1aedc72a3c1a407a7f0d69aa829a0e8b
DanielSsouto <daniel.souto@usp.com>
2021-05-18 16:32:41 -0300

    Alterações do layout

--- src/components/Instrucoes.js
+++ src/components/Instrucoes.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import TemplateEscuro from './TemplateEscuro';
 import trofeu from '../../public/trofeu.png'
 import teclado from '../../public/teclado.png'
@@ -9,8 +9,8 @@ export default function Instrucoes({ mobile }) {
     const [texto, setTexto] = useState(<div></div>)
 
     const niveis = <div>
-        <span style={{display:"flex"}}>
-            <strong style={{ paddingLeft: "1em", paddingRight: "1em"}} >FASES</strong>
+        <span style={{ display: "flex" }}>
+            <strong style={{ paddingLeft: "1em", paddingRight: "1em" }} >FASES</strong>
             <strong>-</strong>
             <div style={{ paddingLeft: "1em", paddingRight: "1em" }} >a cada fase uma nova palavra é adicionada, tal que na fase 5 o jogador deverá descobrir 5 palavras</div>
         </span>
@@ -18,72 +18,72 @@ export default function Instrucoes({ mobile }) {
 
     const pontuacao = <div>
         <span style={{ display: "flex" }}>
-            <strong  style={{ padding: "1em" }} >PONTUAÇÃO</strong>
+            <strong style={{ padding: "1em" }} >PONTUAÇÃO</strong>
             <strong style={{ paddingTop: "1em" }}>-</strong>
             <div style={{ padding: "1em" }}>é o somatório das pontuações obtidas em cada fase</div>
         </span>
-        <div style={{textAlign:"center", paddingLeft: "2em", paddingRight: "2em" }}>Quanto mais rápido for dada a resposta certa, maior será a pontuação!</div>
+        <div style={{ textAlign: "center", paddingLeft: "2em", paddingRight: "2em" }}>Quanto mais rápido for dada a resposta certa, maior será a pontuação!</div>
     </div>
 
     const comandos = <div>
-        <span style={{display:"flex"}}>
+        <span style={{ display: "flex" }}>
             <strong style={{ padding: "1em" }} >ENTER</strong>
-            <strong style={{ paddingTop: "1em"}}>-</strong>
-            <div style={{padding:"1em"}}>verifica a resposta</div>
+            <strong style={{ paddingTop: "1em" }}>-</strong>
+            <div style={{ padding: "1em" }}>verifica a resposta</div>
 
         </span>
-        <span style={{display:"flex"}}>
-            <strong style={{ paddingLeft: "1em", paddingRight: "1em"}} >SPACE</strong>
+        <span style={{ display: "flex" }}>
+            <strong style={{ paddingLeft: "1em", paddingRight: "1em" }} >SPACE</strong>
             <strong>-</strong>
             <div style={{ paddingLeft: "1em", paddingRight: "1em" }} >embaralha a palavra novamente</div>
         </span>
     </div>
-    
+
     const content =
         <div>
-            <div style={{ padding: "1em",  width: mobile ? "75vw" : "50vw"}}>
+            <div style={{ padding: "1em", width: mobile ? "75vw" : "50vw" }}>
                 <p>O Jogo das Letras é composto por cinco fases, cujo objetivo é: a partir de uma dica, desvendar qual palavra se encontra embaralhada. </p>
                 <p>O usuário inicia o jogo na fase 1 podendo chegar à fase 5. A fase define o número de palavras a serem desembaralhadas.</p>
             </div>
-            
-            <div style={!mobile ? { display : "flex", alignItems:"center", position:"relative", bottom:"2vh", justifyContent:"space-around" } : {}}>
-                <div style={{ display: "flex", justifyContent: "space-around"}}>
-                    <span style={{margin: "0.5em"}}>
-                        <span style={{width:"100%", display:"flex", justifyContent:"center"}}>
-                            <a className="botao-redondo verde" onClick={() => { setCor("rgba(26, 188, 156, 0.2)"); setTexto(niveis)}} onLoad={() => { setCor("rgba(26, 188, 156, 0.2)"); setTexto(niveis)}}>
-                                <img src={trofeu} alt="niveis"/>
-                            </a>                    
-                        </span>
-                        <small style={{display:"flex",justifyContent:"center"}}>Fases</small>
-                    </span>
-                    <span style={{margin: "0.5em", display: mobile ? "none" : "block"}}>
-                        <span style={{width:"100%", display:"flex", justifyContent:"center"}}>
-                            <a className="botao-redondo rosa" onClick={() => { setCor("rgba(251, 132, 148, 0.2)"); setTexto(comandos)}}>
-                                <img src={teclado} alt="niveis"/>
-                            </a>                    
-                        </span>
-                        <small style={{display:"flex",justifyContent:"center"}}>Comandos</small>
-                    </span>
-                    <span style={{margin: "0.5em"}}>
-                        <span style={{width:"100%", display:"flex", justifyContent:"center"}}>
-                            <a className="botao-redondo lilas" onClick={() => { setCor("rgba(123, 121, 241, 0.2)"); setTexto(pontuacao)}}>
-                                <img src={estrela} alt="pontuação"/>
+
+            <div style={!mobile ? { display: "flex", alignItems: "center", position: "relative", bottom: "2vh", justifyContent: "space-around" } : {}}>
+                <div style={{ display: "flex", justifyContent: "space-around" }}>
+                    <span style={{ margin: "0.5em" }}>
+                        <span style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                            <a className="botao-redondo verde" onClick={() => { setCor("rgba(26, 188, 156, 0.2)"); setTexto(niveis) }} onLoad={() => { setCor("rgba(26, 188, 156, 0.2)"); setTexto(niveis) }}>
+                                <img src={estrela} alt="niveis" />
                             </a>
                         </span>
-                        <small style={{display:"flex",justifyContent:"center"}}>Pontuação</small>
+                        <small style={{ display: "flex", justifyContent: "center" }}>Fases</small>
+                    </span>
+                    <span style={{ margin: "0.5em", display: mobile ? "none" : "block" }}>
+                        <span style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                            <a className="botao-redondo rosa" onClick={() => { setCor("rgba(251, 132, 148, 0.2)"); setTexto(comandos) }}>
+                                <img src={teclado} alt="niveis" />
+                            </a>
+                        </span>
+                        <small style={{ display: "flex", justifyContent: "center" }}>Comandos</small>
+                    </span>
+                    <span style={{ margin: "0.5em" }}>
+                        <span style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                            <a className="botao-redondo lilas" onClick={() => { setCor("rgba(123, 121, 241, 0.2)"); setTexto(pontuacao) }}>
+                                <img src={trofeu} alt="pontuação" />
+                            </a>
+                        </span>
+                        <small style={{ display: "flex", justifyContent: "center" }}>Pontuação</small>
                     </span>
                 </div>
-                <div style={{width: mobile ? "75vw" : "25vw", height: mobile ? "20vh" : "25vh", backgroundColor:cor,position:"relative",borderRadius:"16px", display:"flex", justifyContent:"center", alignItems:"center", paddingLeft:"1vh", paddingRight:"1vh"}}>
+                <div style={{ width: mobile ? "75vw" : "25vw", height: mobile ? "20vh" : "25vh", backgroundColor: cor, position: "relative", borderRadius: "16px", display: "flex", justifyContent: "center", alignItems: "center", paddingLeft: "1vh", paddingRight: "1vh" }}>
                     {texto}
                 </div>
             </div>
         </div>
 
-    
-    return (    
+
+    return (
         <div>
-            <TemplateEscuro mobile={mobile} id="INSTRUÇÕES" font={"Luckiest Guy"} size="2.3em" bolinhas={false }/>
+            <TemplateEscuro mobile={mobile} id="INSTRUÇÕES" font={"Luckiest Guy"} size="2.3em" bolinhas={false} />
             {content}
         </div>
-    )    
+    )
 }
--- src/components/TemplateEscuro.js
+++ src/components/TemplateEscuro.js
@@ -2,132 +2,132 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import seta from '../../public/seta.png'
 
-export default class TemplateEscuro extends Component{
-    
+export default class TemplateEscuro extends Component {
+
 
     render() {
-        var um= "#FFF", dois= "#FFF", tres= "#FFF", quatro= "#FFF", cinco= "#FFF", bolinhas;
+        var um = "#FFF", dois = "#FFF", tres = "#FFF", quatro = "#FFF", cinco = "#FFF", bolinhas;
         if (this.props.level === 1) {
             um = "#FCCF44";
-            bolinhas = <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:um}}></div>
+            bolinhas = <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: um }}></div>
         }
         else if (this.props.level === 2) {
             if (this.props.fase === 1) {
                 um = "#FCCF44"
-                dois = "#Fff"; 
+                dois = "#Fff";
             }
             else {
                 um = "#6DD0BC"
-                dois = "#FCCF44";                
+                dois = "#FCCF44";
             }
-             bolinhas =
-            <div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:um}}></div>
-                <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: dois }}></div>
-            </div>
+            bolinhas =
+                <div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: um }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: dois }}></div>
+                </div>
         }
         else if (this.props.level === 3) {
             if (this.props.fase === 1) {
                 um = "#FCCF44"
                 dois = "#fff"
-                tres = "#fff";                
+                tres = "#fff";
             }
             else if (this.props.fase === 2) {
                 um = "#6DD0BC"
                 dois = "#FCCF44"
-                tres = "#fff";                
+                tres = "#fff";
             }
             else if (this.props.fase === 3) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
-                tres = "#FCCF44";                
+                tres = "#FCCF44";
             }
             bolinhas =
-            <div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:um}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:dois}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:tres}}></div>
-            </div> 
+                <div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: um }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: dois }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: tres }}></div>
+                </div>
         }
         else if (this.props.level === 4) {
             if (this.props.fase === 1) {
                 um = "#FCCF44"
                 dois = "#fff"
-                tres = "#fff"; 
+                tres = "#fff";
                 quatro = "#fff";
             }
             else if (this.props.fase === 2) {
                 um = "#6DD0BC"
                 dois = "#FCCF44"
-                tres = "#fff";     
+                tres = "#fff";
                 quatro = "#fff";
             }
             else if (this.props.fase === 3) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
-                tres = "#FCCF44";              
+                tres = "#FCCF44";
                 quatro = "#fff";
             }
-            else if(this.props.fase === 4){
+            else if (this.props.fase === 4) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
                 tres = "#6DD0BC"
                 quatro = "#FCCF44";
             }
             bolinhas =
-            <div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:um}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:dois}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:tres}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:quatro}}></div>
-            </div> 
+                <div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: um }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: dois }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: tres }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: quatro }}></div>
+                </div>
         }
         else if (this.props.level === 5) {
             if (this.props.fase === 1) {
                 um = "#FCCF44"
                 dois = "#fff"
-                tres = "#fff"; 
+                tres = "#fff";
                 quatro = "#fff";
                 cinco = "#FFF";
             }
             else if (this.props.fase === 2) {
                 um = "#6DD0BC"
                 dois = "#FCCF44"
-                tres = "#fff";     
+                tres = "#fff";
                 quatro = "#fff";
                 cinco = "#FFF";
             }
             else if (this.props.fase === 3) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
-                tres = "#FCCF44";              
+                tres = "#FCCF44";
                 quatro = "#fff";
                 cinco = "#FFF";
             }
-            else if(this.props.fase === 4){
+            else if (this.props.fase === 4) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
                 tres = "#6DD0BC"
                 quatro = "#FCCF44";
                 cinco = "#FFF";
             }
-            else if(this.props.fase === 5){
+            else if (this.props.fase === 5) {
                 um = "#6DD0BC"
                 dois = "#6DD0BC"
                 tres = "#6DD0BC"
-                quatro ="#6DD0BC"
+                quatro = "#6DD0BC"
                 cinco = "#FCCF44";
             }
             bolinhas =
-            <div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:um}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:dois}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:tres}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:quatro}}></div>
-                <div style={{borderRadius:"50px", width:"10px", height:"10px", margin:"4px", backgroundColor:cinco}}></div>
-            </div> 
-        } 
-        
+                <div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: um }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: dois }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: tres }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: quatro }}></div>
+                    <div style={{ borderRadius: "50px", width: "10px", height: "10px", margin: "4px", backgroundColor: cinco }}></div>
+                </div>
+        }
+
         var subtitle = false
         if (this.props.id === "JOGO DAS LETRAS")
             subtitle = true
@@ -136,48 +136,48 @@ export default class TemplateEscuro extends Component{
             if (this.props.bolinhas) {
                 return (
                     <div className="enunciado mobile">
-                        <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width:"90%" }}>{this.props.id}</p>
+                        <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width: "90%" }}>{this.props.id}</p>
                         <div>
                             {bolinhas}
                         </div>
-                    </div>                    
+                    </div>
                 )
             }
             else if (subtitle) {
-                if (window.innerWidth < 500){
+                if (window.innerWidth < 500) {
                     return (
-                        <div  style={{display:" flex", alignItems: "center"}} className="enunciado mobile inicial">
-                                <div style={{ top: "20%", position: "relative"}}>
-                                    <div style={{ display: "flex", justifyContent: "center", alignItems:"center" }}>
-                                        <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width: "60%", lineHeight: "1", position: 'relative', bottom: "0.5em" }}>{this.props.id}</p>
-                                    </div>
-                                    <p style={{ position: 'relative', bottom: "4em" , fontStyle:"italic"}}>Aedes aegypti</p>
-                                </div>                    
+                        <div style={{ display: " flex", alignItems: "center" }} className="enunciado mobile inicial">
+                            <div style={{ top: "20%", position: "relative" }}>
+                                <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                    <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width: "60%", lineHeight: "1", position: 'relative', bottom: "0.5em" }}>{this.props.id}</p>
+                                </div>
+                                <p style={{ position: 'relative', bottom: "4em", fontStyle: "italic" }}>Jardim Medicinal</p>
+                            </div>
                         </div>
                     )
                 }
-                else{
+                else {
                     return (
-                        <div  style={{display:" flex", alignItems: "center"}} className="enunciado mobile inicial">
-                            <div style={{ top: "20%", position: "relative"}}>
-                                <div style={{ display: "flex", justifyContent: "center", alignItems:"center" }}>
-                                    <p style={{ fontFamily: this.props.font, fontSize: this.props.size, lineHeight: "1", position: 'relative', bottom: "0.5em"}}>{this.props.id}</p>
+                        <div style={{ display: " flex", alignItems: "center" }} className="enunciado mobile inicial">
+                            <div style={{ top: "20%", position: "relative" }}>
+                                <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                                    <p style={{ fontFamily: this.props.font, fontSize: this.props.size, lineHeight: "1", position: 'relative', bottom: "0.5em" }}>{this.props.id}</p>
                                 </div>
-                                <p style={{ position: 'relative',  bottom: "4em"  , fontStyle:"italic"}}>Aedes aegypti</p>
+                                <p style={{ position: 'relative', bottom: "4em", fontStyle: "italic" }}>Jardim medicinal</p>
                             </div>
-                        
+
                         </div>
                     )
                 }
             }
-            else{    
+            else {
                 return (
                     <div className="enunciado mobile">
                         <Link to={{
-                            pathname:'inicial',
+                            pathname: 'inicial',
                         }}>
-                            <div style={{position:"relative", right:"5vw", width:"30px"}}>
-                                <img src={seta} alt="seta"/>
+                            <div style={{ position: "relative", right: "5vw", width: "30px" }}>
+                                <img src={seta} alt="seta" />
                             </div>
                         </Link>
                         <p style={{ fontFamily: this.props.font, fontSize: this.props.size, lineHeight: "1" }}>{this.props.id}</p>
@@ -189,19 +189,19 @@ export default class TemplateEscuro extends Component{
             if (this.props.bolinhas) {
                 return (
                     <div className="enunciado desktop">
-                        <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width:"90%"  }}>{this.props.id}</p>        
+                        <p style={{ fontFamily: this.props.font, fontSize: this.props.size, width: "90%" }}>{this.props.id}</p>
                         <div>
                             {bolinhas}
                         </div>
                     </div>
                 )
             }
-            else if (subtitle){
+            else if (subtitle) {
                 return (
                     <div className="enunciado desktop inicial">
-                        <p style={{ fontFamily: this.props.font, fontSize: '5em'}}>{this.props.id}</p>
-                        <p style={{fontSize: '2em', bottom:"1em", position:"relative", fontStyle:"italic"}}>Aedes aegypti</p>
-                    
+                        <p style={{ fontFamily: this.props.font, fontSize: '5em' }}>{this.props.id}</p>
+                        <p style={{ fontSize: '2em', bottom: "1em", position: "relative", fontStyle: "italic" }}>Jardim medicinal</p>
+
                     </div>
                 )
             }
@@ -209,10 +209,10 @@ export default class TemplateEscuro extends Component{
                 return (
                     <div className="enunciado desktop">
                         <Link to={{
-                                    pathname:'inicial',
+                            pathname: 'inicial',
                         }}>
-                            <div style={{position:"relative", right:"15vw"}}>
-                                <img src={seta} alt="seta" style={{ width: "30px" }}/>
+                            <div style={{ position: "relative", right: "15vw" }}>
+                                <img src={seta} alt="seta" style={{ width: "30px" }} />
                             </div>
                         </Link>
                         <p style={{ fontFamily: this.props.font, fontSize: this.props.size }}>{this.props.id}</p>
